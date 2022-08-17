0211670cd46909d987ebbaf05fe48fa7d316b4c0
Michael Coppola <m18coppola@pop-os.localdomain>
2022-08-17 01:06:31 -0400

    Added file reading capability as commandline argument, added puzzle files.

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.lang.IllegalArgumentException;
+
 /*
  * Sudoku Solver
  * 
  * Michael Coppola
  */
 class SudokuSolver {
-    public static void main(String args[]) {
-      System.out.println("Hello world!"); 
+    public static void main(String args[]) throws IOException {
+        if (args.length != 1) {
+            throw new IllegalArgumentException("Usage: java -jar SudokuSolver filename");
+        }
+
+        try (Reader puzzleFile = new FileReader(args[0]);
+            BufferedReader puzzleReader = new BufferedReader(puzzleFile)) {
+                int input;
+                input = puzzleReader.read();
+                while (input != -1) {
+                    if (!Character.isWhitespace((char)input))
+                    System.out.println((char)input);
+                    input = puzzleReader.read();
+                }
+            }
+        
     }
+    
 }
